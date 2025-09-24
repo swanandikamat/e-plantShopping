c011d35809350f09d1609fbe13ddd8666ebf65af
swanandikamat5@gmail.com <swanandikamat5@gmail.com>
2025-09-24 09:24:52 -0400

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/paradise-nursery-app", // Your GitHub repo name
   plugins: [react()],
-})
+});
