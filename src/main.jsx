112316ce52244d6be54420b9e1891c13628b4d25
swanandikamat5@gmail.com <swanandikamat5@gmail.com>
2025-09-24 09:16:33 -0400

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux"; // ✅ Import Provider
+import store from "./store.js"; // ✅ Import Redux store
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
+    <Provider store={store}> {/* ✅ Wrap App with Provider */}
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
